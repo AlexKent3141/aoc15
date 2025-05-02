b598ccf3e76a23ab0b5d850b9a149bc7c8dbbb6a
Alex Kent <alex.kent1@hotmail.com>
2025-05-02 11:29:30 +0800

    Improve timing script to keep track of total time spent.

--- time.sh
+++ time.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+total=0
 for d in $(seq 1 25)
 do
   echo "Timing day$d"
-  cd d$d
-  hyperfine ./d$d
-  cd ..
+  pushd d$d
+  hyperfine ./d$d --export-json ./tmp.json
+  res=`cat ./tmp.json | jq ".results[0].mean"`
+  rm ./tmp.json
+  echo "Time taken for day$d: $res"
+  total=`awk "BEGIN{ print $total + $res }"`
+  popd
 done
+
+echo "Total: $total"
